84d81588dd2f2ab5bc18ee0fb2b52b77b33243c3
José Leonardo <leodkvt@gmail.com>
2023-08-31 08:30:31 -0300

    Realizado melhorias e correções

--- js/worker.js
+++
@@ -1,8 +0,0 @@
-// This is the worker thread
-addEventListener("message", function (event) {
-  console.log(event.data);
-  // Send a message back to the main thread
-  postMessage({
-    message: "Hello, main thread!",
-  });
-});
